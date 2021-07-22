7feb134dcda8bb2704485a9ff0bf2eece4ab0446
samvitmukherjee <55993846+samvitmukherjee@users.noreply.github.com>
2021-07-22 20:53:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Attandance-System-Using-Facial-Recognition
+# faceRecognition
+
+
+
+Technology used (Libraries):
+- tkinter as tk
+- tkinter import Message, Text
+- cv2, os
+- shutil
+- csv
+- numpy 
+- from PIL import ImageTk,Image
+- pandas 
+- datetime
+- time
+- from itertools import count
+- from functools import partial
+
+Here we were working on Face recognition based Attendance Management System by using OpenCV(Python). One can mark thier attendance by simply facing the camera. 
+
+How it works :
+
+1. Run only the " ".py file.
+2. Enter the "username" and "password" as "admin" and "admin@123" respectively and press "Login".
+3. Fill in the required details in the gui that opens up.
+4. Click on "Take Images" to register yourself.
+5. Click on "Train Images" to train the dataset.
+6. Click on "Detect" to recognize the image and mark the attendance.
+
+Note : -Please make sure you are not sitting in a very dark environment.
+       -While training give different profiles of your face for better training
+       -Wait patiently, till the camera opens up, sometimes takes a bit of time to open up the camera depending open the computers computational power
+       -Wait patiently till it train the images. Huge no of images takes a bit of time to train.
+   
+Additional Steps:
+    - "" folder contains the images who's faces were not recognized
+    - "" constists the dataset of all the students whhich stores teh students Name and ID
+    - "" consists of all the attandance taken so far  
+
+thanks:
+Ashish Dubey
---
+++ test2.py
@@ -0,0 +1,332 @@
+import tkinter as tk
+from tkinter import Message ,Text
+import cv2,os
+import shutil
+import csv
+import numpy as np
+from itertools import count
+#from string import *
+#from string import count
+#from PIL import Image, ImageTk
+import PIL.Image
+from PIL import ImageTk
+import pandas as pd
+import datetime
+import time
+#------------------------------
+from tkinter import *
+from functools import partial
+
+
+
+#window
+admin_username = "admin"
+admin_password = "admin@123"
+
+
+def validateLogin(username, password):
+    a = username.get()
+    b = password.get()
+    if (a == admin_username and b == admin_password):
+        accepted()
+    else:
+        print("INCORRECT CREDENTIALS")
+        tkWindow.destroy()
+
+    # print("username entered :", username.get())
+    # print("password entered :", password.get())
+    # return
+
+
+# window
+tkWindow = Tk()
+tkWindow.geometry('400x150')
+tkWindow.title('Tkinter Login Form - pythonexamples.org')
+
+# username label and text entry box
+usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)
+
+# password label and password entry box
+passwordLabel = Label(tkWindow, text="Password").grid(row=1, column=0)
+password = StringVar()
+passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)
+
+validateLogin = partial(validateLogin, username, password)
+
+# login button
+loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)
+
+
+def accepted():
+    print("SUCCESSFULLY LOGGED IN")
+
+    window = tk.Tk()
+    # helv36 = tk.Font(family='Helvaetica', size=36, weight='bold')
+    window.title("Attendance System Using Facial Recognition")
+
+    dialog_title = 'QUIT'
+    dialog_text = 'Are you sure?'
+    # answer = messagebox.askquestion(dialog_title, dialog_text)
+
+    # window.geometry('1280x720')
+
+    background_image = ImageTk.PhotoImage(file=r'Background_Image_C.png',master=window)
+    background_label = tk.Label(window, image=background_image)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # window.attributes('-fullscreen', True)
+
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(0, weight=1)
+
+    # path = "profile.jpg"
+
+    # Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+    # img = ImageTk.PhotoImage(Image.open(path))
+
+    # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+    # panel = tk.Label(window, image = img)
+
+    # panel.pack(side = "left", fill = "y", expand = "no")
+
+    # cv_img = cv2.imread("img541.jpg")
+    # x, y, no_channels = cv_img.shape
+    # canvas = tk.Canvas(window, width = x, height =y)
+    # canvas.pack(side="left")
+    # photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(cv_img))
+    # Add a PhotoImage to the Canvas
+    # canvas.create_image(0, 0, image=photo, anchor=tk.NW)
+
+    # msg = Message(window, text='Hello, world!')
+
+    # Font is a tuple of (font_family, size_in_points, style_modifier_string)
+
+
+
+
+
+
+
+    message = tk.Label(window, text="Attendance System Using Facial Recognition", bg='white', fg="black",
+                       height=2, font=('Algerian', 41, 'bold underline'))
+
+    message.place(x=178, y=20)
+
+    img = ImageTk.PhotoImage(PIL.Image.open('DTU_Logo.png'),master=window)
+
+    # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+    panel = tk.Label(window, image=img, width=170)
+    panel.place(x=10, y=20)
+
+    lbl = tk.Label(window, text="Enter ID", width=20, height=2, fg="red", bg="yellow", font=('Calbiri', 15, ' bold '))
+    lbl.place(x=400, y=200)
+
+    txt = tk.Entry(window, width=20, bg="yellow", fg="red", font=('Calbiri', 15, ' bold '))
+    txt.place(x=700, y=215)
+
+    lbl2 = tk.Label(window, text="Enter Name", width=20, fg="red", bg="yellow", height=2,
+                    font=('Calbiri', 15, ' bold '))
+    lbl2.place(x=400, y=300)
+
+    txt2 = tk.Entry(window, width=20, bg="yellow", fg="red", font=('Calbiri', 15, ' bold '))
+    txt2.place(x=700, y=315)
+
+    lbl3 = tk.Label(window, text="Notification:", width=20, fg="red", bg="yellow", height=2,
+                    font=('Calbiri', 15, ' bold underline '))
+    lbl3.place(x=400, y=400)
+
+    message = tk.Label(window, text="", bg="yellow", fg="red", width=40, height=2, activebackground="yellow",
+                       font=('Calbiri', 15, ' bold '))
+    message.place(x=700, y=400)
+
+    lbl3 = tk.Label(window, text="Attendance Record:", width=20, fg="red", bg="yellow", height=2,
+                    font=('Calbiri', 15, ' bold  underline'))
+    lbl3.place(x=400, y=650)
+
+    message2 = tk.Label(window, text="", fg="red", bg="yellow", activeforeground="green", width=35, height=2,
+                        font=('Calbiri', 15, ' bold '))
+    message2.place(x=700, y=650)
+
+    def clear():
+        txt.delete(0, 'end')
+        res = ""
+        message.configure(text=res)
+
+    def clear2():
+        txt2.delete(0, 'end')
+        res = ""
+        message.configure(text=res)
+
+    def is_number(s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            pass
+
+        try:
+            import unicodedata
+            unicodedata.numeric(s)
+            return True
+        except (TypeError, ValueError):
+            pass
+
+        return False
+
+    def TakeImages():
+        Id = (txt.get())
+        name = (txt2.get())
+        if (is_number(Id) and name.isalpha()):
+            cam = cv2.VideoCapture(0)
+            harcascadePath = "haarcascade_frontalface_default.xml"
+            detector = cv2.CascadeClassifier(harcascadePath)
+            sampleNum = 0
+            while (True):
+                ret, img = cam.read()
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = detector.detectMultiScale(gray, 1.3, 5)
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                    # incrementing sample number
+                    sampleNum = sampleNum + 1
+                    # saving the captured face in the dataset folder TrainingImage
+                    cv2.imwrite("TrainingImage\ " + name + "." + Id + '.' + str(sampleNum) + ".jpg",
+                                gray[y:y + h, x:x + w])
+                    # display the frame
+                    cv2.imshow('frame', img)
+                # wait for 100 miliseconds
+                if cv2.waitKey(100) & 0xFF == ord('q'):
+                    break
+                # break if the sample number is morethan 100
+                elif sampleNum > 60:
+                    break
+            cam.release()
+            cv2.destroyAllWindows()
+            res = "Images Saved for ID : " + Id + " Name : " + name
+            row = [Id, name]
+            with open('StudentDetails\StudentDetails.csv', 'a+') as csvFile:
+                writer = csv.writer(csvFile)
+                writer.writerow(row)
+            csvFile.close()
+            message.configure(text=res)
+        else:
+            if (is_number(Id)):
+                res = "Enter Alphabetical Name"
+                message.configure(text=res)
+            if (name.isalpha()):
+                res = "Enter Numeric Id"
+                message.configure(text=res)
+
+    def TrainImages():
+        recognizer = cv2.face_LBPHFaceRecognizer.create()  # recognizer = cv2.face.LBPHFaceRecognizer_create()#$cv2.createLBPHFaceRecognizer()
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        faces, Id = getImagesAndLabels("TrainingImage")
+        recognizer.train(faces, np.array(Id))
+        recognizer.save("TrainingImageLabel\Trainner.yml")
+        res = "Image Trained"  # +",".join(str(f) for f in Id)
+        message.configure(text=res)
+
+    def getImagesAndLabels(path):
+        # get the path of all the files in the folder
+        imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+        # print(imagePaths)
+
+        # create empth face list
+        faces = []
+        # create empty ID list
+        Ids = []
+        # now looping through all the image paths and loading the Ids and the images
+        for imagePath in imagePaths:
+            # loading the image and converting it to gray scale
+            pilImage = PIL.Image.open(imagePath).convert('L')
+            # Now we are converting the PIL image into numpy array
+            imageNp = np.array(pilImage, 'uint8')
+            # getting the Id from the image
+            Id = int(os.path.split(imagePath)[-1].split(".")[1])
+            # extract the face from the training image sample
+            faces.append(imageNp)
+            Ids.append(Id)
+        return faces, Ids
+
+    def TrackImages():
+        recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
+        recognizer.read("TrainingImageLabel\Trainner.yml")
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        faceCascade = cv2.CascadeClassifier(harcascadePath);
+        df = pd.read_csv("StudentDetails\StudentDetails.csv")
+        cam = cv2.VideoCapture(0)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        col_names = ['Id', 'Name', 'Date', 'Time']
+        attendance = pd.DataFrame(columns=col_names)
+        while True:
+            ret, im = cam.read()
+            gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(im, (x, y), (x + w, y + h), (225, 0, 0), 2)
+                Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
+                if (conf < 50):
+                    ts = time.time()
+                    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                    aa = df.loc[df['Id'] == Id]['Name'].values
+                    tt = str(Id) + "-" + str(aa)
+                    attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+
+                else:
+                    Id = 'Unknown'
+                    tt = str(Id)
+                if (conf > 75):
+                    noOfFile = len(os.listdir("ImagesUnknown")) + 1
+                    cv2.imwrite("ImagesUnknown\Image" + str(noOfFile) + ".jpg", im[y:y + h, x:x + w])
+                cv2.putText(im, str(tt), (x, y + h), font, 1, (255, 255, 255), 2)
+            attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
+            cv2.imshow('im', im)
+            if (cv2.waitKey(1) == ord('q')):
+                break
+        ts = time.time()
+        date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+        timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+        Hour, Minute, Second = timeStamp.split(":")
+        fileName = "Attendance\Attendance_" + date + "_" + Hour + "-" + Minute + "-" + Second + ".csv"
+        attendance.to_csv(fileName, index=False)
+        cam.release()
+        cv2.destroyAllWindows()
+        # print(attendance)
+        res = attendance
+        message2.configure(text=res)
+
+    clearButton = tk.Button(window, text="Clear", command=clear, fg="red", bg="yellow", width=20, height=1,
+                            activebackground="Red", font=('Calbiri', 15, ' bold '))
+    clearButton.place(x=950, y=200)
+    clearButton2 = tk.Button(window, text="Clear", command=clear2, fg="red", bg="yellow", width=20, height=1,
+                             activebackground="Red", font=('Calbiri', 15, ' bold '))
+    clearButton2.place(x=950, y=300)
+    takeImg = tk.Button(window, text="Take Images", command=TakeImages, fg="red", bg="green", width=20, height=1,
+                        activebackground="Red", font=('Calbiri', 15, ' bold '))
+    takeImg.place(x=200, y=530)
+    trainImg = tk.Button(window, text="Train Images", command=TrainImages, fg="red", bg="green", width=20, height=1,
+                         activebackground="Red", font=('Calbiri', 15, ' bold '))
+    trainImg.place(x=500, y=530)
+    trackImg = tk.Button(window, text="Track Images", command=TrackImages, fg="red", bg="green", width=20, height=1,
+                         activebackground="Red", font=('Calbiri', 15, ' bold '))
+    trackImg.place(x=800, y=530)
+    quitWindow = tk.Button(window, text="Quit", command=window.destroy, fg="red", bg="green", width=20, height=1,
+                           activebackground="Red", font=('Calbiri', 15, ' bold '))
+    quitWindow.place(x=1100, y=530)
+    copyWrite = tk.Text(window, background=window.cget("background"), borderwidth=0,
+                        font=('Calbiri', 12, 'bold underline'))
+    copyWrite.tag_configure("superscript", offset=10)
+    copyWrite.insert("insert", "Developed By: Rishabh And Samvit")
+    copyWrite.configure(state="disabled", fg="red", bg="yellow", width=31)
+    copyWrite.pack(side="left")
+    copyWrite.place(x=1230, y=780)
+
+    window.mainloop()
+
+
+
+tkWindow.mainloop()
